a672d923672d9bc21cef48dc39df3b6ebdb97cf8
Pito Salas <pitosalas@gmail.com>
2017-02-01 20:06:08 -0500

    Minor

--- src/edu/brandeis/spacegame/SpaceGame.java
+++ src/edu/brandeis/spacegame/SpaceGame.java
@@ -2,9 +2,8 @@ package edu.brandeis.spacegame;
 
 public class SpaceGame {
   public static void main(String[] args) {
-    int n = 15;
     Game board = new Game();
-    board.initGame(n);
+    board.initGame(Constants.SIZE);
     board.gameLoop();
     board.endGame();
   }
